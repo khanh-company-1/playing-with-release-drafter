7058f4e0ff942fc2783bd81cbf5a8fd9b15e6164
Khanh Nguyen <119989010+ndkhanh-axonivy@users.noreply.github.com>
2024-12-27 13:59:31 +0700

    Fix-1-12 (#4)

--- java/App.java
+++ java/App.java
@@ -2,7 +2,7 @@ package com.khanhcompany1.playingwithreleasedrafter;
 
 public class App {
     public static void main(String[] args) {
-        String bug = "bug-2";
+        String bug = "bug-1-12";
         System.out.println("Fixing bug: " + bug);
         System.out.println("Create release 12.0.0");
         System.out.println("Hello, Release Drafter Action! Here version 10.0.0");
