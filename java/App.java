437a41d086877bab1426e4dd8bcaca0fce2a528c
Khanh Nguyen <119989010+ndkhanh-axonivy@users.noreply.github.com>
2024-12-27 13:57:14 +0700

    Update App.java (#3)

--- java/App.java
+++ java/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) {
         String bug = "bug-2";
         System.out.println("Fixing bug: " + bug);
+        System.out.println("Create release 12.0.0");
         System.out.println("Hello, Release Drafter Action! Here version 10.0.0");
     }
 }
