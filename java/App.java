da911290c2550529926b48ab9927ade6d51a1c3a
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-12-27 10:30:32 +0700

    Init project

---
+++ java/App.java
@@ -0,0 +1,7 @@
+package com.khanhcompany1.playingwithreleasedrafter;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Release Drafter Action! Here version 10.0.0");
+    }
+}
