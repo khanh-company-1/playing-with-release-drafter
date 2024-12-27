ed54720a66aea5154aa7d94cda0e7d29c5e72cd2
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-12-27 14:23:09 +0700

    Update App.java

--- java/App.java
+++ java/App.java
@@ -2,7 +2,7 @@ package com.khanhcompany1.playingwithreleasedrafter;
 
 public class App {
     public static void main(String[] args) {
-        String bug = "bug-10-3";
+        String bug = "bug-10-4";
         System.out.println("Fixing bug: " + bug);
         System.out.println("Hello, Release Drafter Action! Here version 10.0.0");
     }
