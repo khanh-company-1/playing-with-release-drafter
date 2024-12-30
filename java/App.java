bfbd5040c1303093aa38cae5688ec9f960e85b5b
Khanh Nguyen <khanh.nguyenduy@axonactive.com>
2024-12-30 15:16:29 +0700

    Update App.java

--- java/App.java
+++ java/App.java
@@ -2,7 +2,7 @@ package com.khanhcompany1.playingwithreleasedrafter;
 
 public class App {
     public static void main(String[] args) {
-        String bug = "bug-main-1";
+        String bug = "bug-main-2";
         System.out.println("Fixing bug: " + bug);
         System.out.println("Create release 12.0.0");
         System.out.println("Hello, Release Drafter Action! Here version 10.0.0");
